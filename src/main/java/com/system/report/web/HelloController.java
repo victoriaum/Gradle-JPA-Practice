a0426fbe34c533cfd880dbb7f7482f292f25234a
victoriaum <y.ilpyo95@gmail.com>
2021-11-24 19:59:35 +0900

    dto testcode 작성

--- src/main/java/com/system/report/web/HelloController.java
+++ src/main/java/com/system/report/web/HelloController.java
@@ -1,12 +1,20 @@
 package com.system.report.web;
 
+import com.system.report.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
   @GetMapping("/hello")
   public String hello(){
     return "hello";
+  }
+
+  @GetMapping("/hello/dto")
+  public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+    return new HelloResponseDto(name, amount);
   }
 }
